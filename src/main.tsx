f283dbd887d0e6cc0c8031439fa489025ba4ecbc
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-06 02:49:04 +0200

    feat: add pages to router ✨

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CreateEmployee from './pages/CreateEmployee'
+import EmployeeList from './pages/EmployeeList'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<></>} />
+        <Route path="/" element={<CreateEmployee />} />
+        <Route path="/employee-list" element={<EmployeeList />} />
       </Routes>
     </Router>
   </React.StrictMode>
