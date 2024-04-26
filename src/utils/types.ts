a5ec92f234d475aea815f87feeb750e3650e92af
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-27 01:39:40 +0200

    feat: add Employee type ✨

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,15 @@ export type State = {
   name: string
   abbreviation: string
 }
+
+export type Employee = {
+  firstName: string
+  lastName: string
+  birthDate: Date
+  startDate: Date
+  addressStreet: string
+  addressCity: string
+  addressState: string
+  addressZip: number
+  department: string
+}
