9f0ac57a9c8e3cb620537792e5830d8c81c17af4
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-27 02:08:12 +0200

    feat: connect zustand to Redux devtools ✨

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,8 +1,11 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 import { Employee, Store } from './types'
 
-export const useEmployeeStore = create<Store>((set) => ({
-  employees: [],
-  addEmployee: (employee: Employee) =>
-    set((state) => ({ employees: [...state.employees, employee] })),
-}))
+export const useEmployeeStore = create(
+  devtools<Store>((set) => ({
+    employees: [],
+    addEmployee: (employee: Employee) =>
+      set((state) => ({ employees: [...state.employees, employee] })),
+  }))
+)
