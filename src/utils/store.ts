ed10b5a4cfda52863e3d05c12e67e38bfc349ab8
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-27 07:18:30 +0200

    feat: add modal controls in the store ✨

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -5,7 +5,12 @@ import { Employee, Store } from './types'
 export const useEmployeeStore = create(
   devtools<Store>((set) => ({
     employees: [],
+    modalVisible: false,
+    modalContent: '',
     addEmployee: (employee: Employee) =>
       set((state) => ({ employees: [...state.employees, employee] })),
+    showModal: (text: string) =>
+      set(() => ({ modalVisible: true, modalContent: text })),
+    hideModal: () => set(() => ({ modalVisible: false })),
   }))
 )
