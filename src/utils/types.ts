54d61548a8011b199841bf077f413f210d90317a
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-07 20:01:57 +0200

    feat: add Employee type guard ✨

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,8 +6,8 @@ export type State = {
 export type Employee = {
   firstname: string
   lastname: string
-  birthDate: Date
-  startDate: Date
+  birthDate: string
+  startDate: string
   addressStreet: string
   addressCity: string
   addressState: string
@@ -22,4 +22,18 @@ export type Store = {
   addEmployee: (employee: Employee) => void
   showModal: (text: string) => void
   hideModal: () => void
+}
+
+export function isEmployee(data: unknown): data is Employee {
+  const employee = data as Employee
+  if (!('firstname' in employee)) return false
+  if (!('lastname' in employee)) return false
+  if (!('birthDate' in employee)) return false
+  if (!('startDate' in employee)) return false
+  if (!('addressStreet' in employee)) return false
+  if (!('addressCity' in employee)) return false
+  if (!('addressState' in employee)) return false
+  if (!('addressZip' in employee)) return false
+  if (!('department' in employee)) return false
+  return true
 }
