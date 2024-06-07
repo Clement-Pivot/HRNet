6eeb1d429ca0d0d891944b82c35fe8e3c56fd740
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-07 21:14:21 +0200

    refactor: move bootstrap'css import to main.tsx 📦

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CreateEmployee from './pages/CreateEmployee'
 import EmployeeList from './pages/EmployeeList'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
