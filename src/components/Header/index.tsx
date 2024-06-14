97b692f4201a835827ed4e3c4516f416ed4af558
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-14 04:57:24 +0200

    feat: change link and title when changing page ✨

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Nav } from 'react-bootstrap'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Header() {
+  const location = useLocation()
+
   return (
-    <>
+    <Nav className="justify-content-between align-items-center mb-5">
       <h1>HRnet</h1>
-      <Link to="/employee-list">View Current Employees</Link>
-    </>
+      {location.pathname == '/' ? (
+        <>
+          <Link to="/employee-list">View Current Employees</Link>
+          <title>HRnet - Create Employee</title>
+        </>
+      ) : (
+        <>
+          <Link to="/">Create Employee</Link>
+          <title>HRnet - Employees List</title>
+        </>
+      )}
+    </Nav>
   )
 }
