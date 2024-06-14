756751e42fb9409928852002b12c8ea91a9b1a19
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-14 04:58:25 +0200

    feat: add store persist with session storage ✨

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,16 +1,22 @@
 import { create } from 'zustand'
-import { devtools } from 'zustand/middleware'
+import { devtools, persist, createJSONStorage } from 'zustand/middleware'
 import { Employee, Store } from './types'
 
 export const useEmployeeStore = create(
-  devtools<Store>((set) => ({
-    employees: [],
-    modalVisible: false,
-    modalContent: '',
-    addEmployee: (employee: Employee) =>
-      set((state) => ({ employees: [...state.employees, employee] })),
-    showModal: (text: string) =>
-      set(() => ({ modalVisible: true, modalContent: text })),
-    hideModal: () => set(() => ({ modalVisible: false })),
-  }))
+  persist(
+    devtools<Store>((set) => ({
+      employees: [],
+      modalVisible: false,
+      modalContent: '',
+      addEmployee: (employee: Employee) =>
+        set((state) => ({ employees: [...state.employees, employee] })),
+      showModal: (text: string) =>
+        set(() => ({ modalVisible: true, modalContent: text })),
+      hideModal: () => set(() => ({ modalVisible: false })),
+    })),
+    {
+      name: 'employees-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
 )
