7a14cbf58c2c31e44f4fd06db31bea1d1b6f1af8
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-07-04 15:27:32 +0200

    fix: try HashRouter for GH pages 🐛

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import CreateEmployee from './pages/CreateEmployee'
 import EmployeeList from './pages/EmployeeList'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -9,7 +9,7 @@ import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router basename="/HRNet/">
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<CreateEmployee />} />
